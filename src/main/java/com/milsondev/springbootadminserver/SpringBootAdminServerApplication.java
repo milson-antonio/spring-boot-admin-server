60c0cb1bb0b7d8c0a98deccdccba6d2dd1ac19e1
milson.antonio <milson.antonio@favendo.com>
2025-07-22 16:56:50 +0200

    added properties

--- src/main/java/com/milsondev/springbootadminserver/SpringBootAdminServerApplication.java
+++ src/main/java/com/milsondev/springbootadminserver/SpringBootAdminServerApplication.java
@@ -1,13 +1,24 @@
 package com.milsondev.springbootadminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringBootAdminServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdminServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
+
+    @Bean
+    public AuditEventRepository auditEventRepository() {
+        return new InMemoryAuditEventRepository();
+    }
+
 
 }
